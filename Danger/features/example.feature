f765b3b1035e220fc03d1f60f0fbd070f30f864b
jeancrb19 <jeancarlos-rb@hotmail.com>
2017-11-29 14:49:20 -0200

    I

---
+++ Danger/features/example.feature
@@ -0,0 +1,7 @@
+#language: pt
+@joaquino
+Funcionalidade: Testar o Joaquino
+  Cenario: Joaquino é top
+    Dado que estou na url "https://www.mercadolivre.com.br/"
+    E pesquiso por "iphone x" na ".nav-search-input"
+    Entao espero que sejam listados iphone x
---
+++ Danger/features/step_definitions/step_definitions.rb
@@ -0,0 +1,16 @@
+Dado(/^que estou na url "([^"]*)"$/) do |arg1|
+  visit(arg1)
+  sleep(2)
+end
+
+Dado(/^pesquiso por "([^"]*)" na "([^"]*)"$/) do |arg1, arg2|
+  find(arg2).set(arg1)
+end
+
+Entao(/^espero que sejam listados iphone x$/) do
+  if has_text?('Iphone X', wait: 50)
+    puts "Sucesso tem iPhone na tela"
+  else
+    raise "Não foram apresentados iphones X"
+  end
+end
